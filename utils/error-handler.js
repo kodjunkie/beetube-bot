fae5dd57edae919ca4f2edc4282466cb5e09ecc9
Lawrence Onah <paplow01@gmail.com>
2020-03-03 11:18:22 +0100

    Added error handler

---
+++ utils/error-handler.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+module.exports = (bot, chatId, error) => {
+	if (error && process.env.NODE_ENV !== "production") {
+		console.error(error);
+	}
+
+	bot.sendMessage(chatId, `\u{26A0} An error occurred, try again \u{26A0}`, {
+		parse_mode: "Markdown",
+	});
+};
