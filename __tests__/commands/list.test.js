615bb5d27af8416a2f7125a4bfc60871a391ba8b
Lawrence Onah <paplow01@gmail.com>
2020-03-03 10:26:56 +0100

    :rotating_light: improve tests

--- __tests__/commands/list.test.js
+++ __tests__/commands/list.test.js
@@ -1,9 +1,9 @@
-const startCommand = require("../../commands/start");
+const listCommand = require("../../commands/list");
 
-it("responds to /start command", () => {
+it("responds to /list command", () => {
 	expect.assertions(3);
 	const message = bot.message();
-	startCommand(bot)(message);
+	listCommand(bot)(message);
 	expect(bot.sendChatAction.mock.calls.length).toBe(1);
 	expect(bot.sendChatAction).toBeCalledWith(message.chat.id, "typing");
 	expect(bot.sendMessage.mock.calls.length).toBe(1);
