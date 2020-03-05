969b9b999ac281991944742df35f4eb7821d6e01
Lawrence Onah <paplow01@gmail.com>
2020-03-05 11:32:17 +0100

    :heavy_plus_sign: added config file

---
+++ config/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+	// TelegramBot config
+	bot: {
+		polling: true,
+		onlyFirstMatch: true,
+	},
+
+	// MongoDB config
+	mongodb: {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+};
