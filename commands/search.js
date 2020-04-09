8b2154e7afcc44407cc02b5d1089fef562b84e16
Lawrence Onah <paplow01@gmail.com>
2020-04-09 18:54:51 +0100

    :heavy_plus_sign: :zap: Added movie search feature

--- commands/search.js
+++ commands/search.js
@@ -1,11 +1,21 @@
 /*
- * Handle "/search [provider] [whatever] -[flag]"
+ * Handle "/search [provider] [whatever] -[server]"
  */
+
+const movieProvider = require("../providers/movie");
+
 module.exports = bot => (message, match) => {
-	// console.log(match.groups);
 	const chatId = message.chat.id;
+	const matches = match.groups;
 	bot.sendChatAction(chatId, "typing");
-	bot.sendMessage(chatId, "This feature will be available soon \u{1F6A7}", {
-		parse_mode: "Markdown",
-	});
+
+	switch (matches.Provider) {
+		case "movies":
+			const params = { query: matches.Query, server: matches.Server || null };
+			const movie = new movieProvider(bot);
+			movie.search(message, params);
+			break;
+		default:
+			bot.sendMessage(chatId, "This feature will be available soon \u{1F6A7}");
+	}
 };
--- providers/movie.js
+++ providers/movie.js
@@ -18,11 +18,10 @@ module.exports = class Movie extends Provider {
 			);
 
 			const { data } = await axios.get(
-				`${process.env.MOVIES_API}list?page=${page}`
+				`${process.env.MOVIES_API}/list?page=${page}`
 			);
 
 			const options = { parse_mode: "Markdown" };
-
 			_.map(data, (movie, i) => {
 				const isLastItem = data.length - 1 === i;
 				/*
@@ -104,5 +103,52 @@ module.exports = class Movie extends Provider {
 		}
 
 		await this.list(message, page);
+	}
+
+	/**
+	 * Search for movies
+	 * @param  {} message
+	 * @param  {} params
+	 */
+	async search({ chat }, params) {
+		try {
+			const { message_id } = await this.bot.sendMessage(
+				chat.id,
+				`\u{1F504} Searching for \`${params.query}\` \u{1F4E1}`
+			);
+
+			const query = params.query.replace(" ", "+");
+			const server = params.server ? `&engine=${params.server}` : "";
+			const { data } = await axios.get(
+				`${process.env.MOVIES_API}/search?query=${query + server}`
+			);
+
+			const options = { parse_mode: "Markdown" };
+			_.map(data, async movie => {
+				if (movie.Size && movie.CoverPhotoLink) {
+					options.reply_markup = JSON.stringify({
+						inline_keyboard: [
+							[{ text: `Download ${movie.Size}`, url: movie.DownloadLink }],
+						],
+					});
+
+					const msg = await this.bot.sendMessage(
+						chat.id,
+						`[\u{1F4C0}](${movie.CoverPhotoLink}) *${movie.Title}*`,
+						options
+					);
+
+					await new Paginator({
+						_id: msg.message_id,
+						type: "movie",
+						user: msg.chat.id,
+					}).save();
+				}
+			});
+
+			this.bot.deleteMessage(chat.id, message_id);
+		} catch (error) {
+			errorHandler(this.bot, chat.id, error);
+		}
 	}
 };
