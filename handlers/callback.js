c8ded6a7cd8369c2fb9be5dabd5cd78fb466ec34
Lawrence Onah <paplow01@gmail.com>
2020-04-10 01:31:33 +0100

    :hammer: :zap: Improved list functionality

--- handlers/callback.js
+++ handlers/callback.js
@@ -4,19 +4,29 @@
 const errorHandler = require("../utils/error-handler");
 const movieProvider = require("../providers/movie");
 
-module.exports = bot => callbackQuery => {
+module.exports = bot => cbq => {
+	const chatId = cbq.message.chat.id;
+	const data = JSON.parse(cbq.data);
+
 	try {
-		const data = JSON.parse(callbackQuery.data);
+		const movie = new movieProvider(bot);
+
 		switch (data.type) {
-			case "movie":
-				const movie = new movieProvider(bot);
-				movie.paginate(callbackQuery.message, data.page);
+			case "list_movies":
+				movie.list(cbq.message);
+				break;
+			case "paginate_movies":
+				movie.paginate(cbq.message, data.page);
 				break;
 			default:
-				console.log("Query Callback: ", callbackQuery);
+				bot.sendMessage(
+					chatId,
+					"This feature will be available soon \u{1F6A7}"
+				);
 		}
 	} catch (error) {
-		errorHandler(bot, callbackQuery.message.chat.id, error);
+		errorHandler(bot, chatId, error);
 	}
-	bot.answerCallbackQuery(callbackQuery.id);
+
+	bot.answerCallbackQuery(cbq.id);
 };
