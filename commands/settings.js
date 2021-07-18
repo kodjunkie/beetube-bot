2a3abf6a0d143d099de2a92e2a60288e3363ed2a
Lawrence Onah <paplow01@gmail.com>
2021-07-18 22:40:55 +0100

    :wrench: Refactoring and enhancements

--- commands/settings.js
+++ commands/settings.js
@@ -1,11 +1,16 @@
 /*
  * Handle settings command
  */
-
+const Paginator = require("../models/paginator");
 const keyboardMarkup = require("../utils/keyboard");
 
 module.exports = bot => async message => {
 	const chatId = message.chat.id;
+	await bot.sendChatAction(chatId, "typing");
+
+	// Remove obsolete db records
+	await Paginator.removeObsoleteRecords();
+
 	await bot.sendMessage(
 		chatId,
 		"\u{1F6A7} This feature will be available soon.",
