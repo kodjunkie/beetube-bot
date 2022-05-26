c594229292f6abccaeef6f0f8d063721a0c14277
Lawrence Onah <paplow01@gmail.com>
2022-05-26 05:46:07 +0100

    MInor fixes

--- commands/about.js
+++ commands/about.js
@@ -10,8 +10,8 @@ module.exports = bot => async message => {
 	const chatId = message.chat.id;
 	await bot.sendMessage(
 		chatId,
-		`\u{2139} *About this bot*
-		\n\u{1F41D} \`${env.BOT_NAME}\` is an open source telegram bot developed to give users access to unlimited free downloads within telegram. 
+		`\u{2139} *About*
+		\n${env.BOT_NAME} is an open source telegram bot developed to give users access to unlimited free downloads within telegram. 
 		\n\`If you find this bot useful, feel free to give us a \u{2B50} on Github.\`
 		\nWant to contribute to the development of this project? PRs are always welcome.
 		\nOfficial channel: @${env.BOT_CHANNEL}\nGithub: [kodjunkie/beetube-bot](${homepage})\nFor bug report create an [issue](${bugs.url})`,
--- utils/error-handler.js
+++ utils/error-handler.js
@@ -4,7 +4,8 @@
  * @param error
  */
 module.exports = (bot, chatId, error) => {
-	if (error && process.env.NODE_ENV !== "production") {
+	// Log errors in development mode
+	if (error && process.env.NODE_ENV.includes("dev")) {
 		console.error(error);
 	}
 	bot.sendMessage(chatId, "\u{1F6AB} An error occurred, try again later.");
