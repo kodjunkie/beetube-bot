091b9021fe8e91139c483e5e798f29024c863f7b
Lawrence Onah <paplow01@gmail.com>
2022-09-16 14:19:19 +0100

    Minor refactoring

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const TelegramBot = require("node-telegram-bot-api");
 TelegramBot.Promise = require("bluebird").config({
 	cancellation: true,
 });
-require("dotenv").config();
 const env = process.env;
 const config = require("./config");
-const commands = require("./commands");
-const handlers = require("./handlers");
+const command = require("./commands");
+const handler = require("./handlers");
 
 const bot = new TelegramBot(env.BOT_TOKEN, config.bot);
 
@@ -15,17 +15,17 @@ mongoose
 	.connect(env.MONGODB_URI, config.mongodb)
 	.then(() => {
 		// Commands
-		bot.onText(/\/start/, commands.start(bot));
-		bot.onText(/about$/i, commands.about(bot));
-		bot.onText(/search$/i, commands.search(bot));
-		bot.onText(/settings$/i, commands.settings(bot));
-		bot.onText(/\/keyboard/, commands.keyboard(bot));
-		bot.onText(/(?<provider>(Movie|Music|Torrent|Anime)$)/, commands.list(bot));
+		bot.onText(/\/start/, command.start(bot));
+		bot.onText(/about$/i, command.about(bot));
+		bot.onText(/search$/i, command.search(bot));
+		bot.onText(/settings$/i, command.settings(bot));
+		bot.onText(/\/keyboard/, command.keyboard(bot));
+		bot.onText(/(?<provider>(Movie|Music|Torrent|Anime)$)/, command.list(bot));
 
 		// Handlers
-		bot.on("callback_query", handlers.callbackQuery(bot));
-		bot.on("polling_error", handlers.botError);
-		bot.on("error", handlers.botError);
+		bot.on("callback_query", handler.callbackQuery(bot));
+		bot.on("polling_error", handler.botError);
+		bot.on("error", handler.botError);
 
 		console.info(`\u{1F41D} ${env.BOT_NAME} started successfully`);
 	})
